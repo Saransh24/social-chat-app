ce7ed9f808363f5309abf30127316a0e98b88645
Saransh <saransh24082002@gmail.com>
2023-06-18 01:17:09 +0530

    Completed frontend and backend of setAvatar page

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,9 +1,10 @@
 // This file handle all the calls which are coming to backend and then sending it to respective functions in userController
-const { register, login } = require("../controllers/userController");
+const { register, login, setAvatar } = require("../controllers/userController");
 
 const router = require("express").Router();
 
 router.post("/register", register);
 router.post("/login", login);
+router.post("/setAvatar/:id", setAvatar);
 
 module.exports = router;
