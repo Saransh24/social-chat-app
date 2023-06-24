a79bbb50a740c1dd1b6926841afb1f866c4c6342
Saransh <saransh24082002@gmail.com>
2023-06-24 18:04:07 +0530

    Completed Chatbox frontend

--- public/src/components/ChatContainer.jsx
+++ public/src/components/ChatContainer.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import styled from "styled-components";
 import Logout from "./Logout";
+import ChatInput from "./ChatInput";
+import Messages from "./Messages";
 
 const ChatContainer = ({ currentChat }) => {
+  const handleSendMsg = async (msg) => {
+    alert(msg);
+  }
+
   return (
     <>
       {currentChat && (
@@ -21,8 +27,8 @@ const ChatContainer = ({ currentChat }) => {
             </div>
             <Logout />
           </div>
-          <div className="chat-messages"></div>
-          <div className="chat-input"></div>
+          <Messages />
+          <ChatInput handleSendMsg={handleSendMsg} />
         </Container>
       )}
     </>
